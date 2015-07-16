5f29ccdf002781508df6d9af2e98df0d541ed0fe
Tzu-Lin Huang <tzhuang@mozilla.com>
2015-07-16 17:04:04 +0800

    Add bower.json and implement tabs(panel)

--- js/app.js
+++ js/app.js
@@ -4,5 +4,50 @@
   var App = function() {
   };
 
+  App.prototype = {
+    classicSection: document.getElementById('classic-api'),
+    bleServerSection: document.getElementById('ble-server-api'),
+    bleClientSection: document.getElementById('ble-client-api'),
+    controlPanel: document.getElementById('control-panel'),
+
+    panelSections: document.getElementsByClassName('panel'),
+    modeButtons: document.getElementsByClassName('mode-button'),
+    mode: document.body.dataset.mode,
+
+    start: function() {
+      var that = this;
+      [].forEach.call(this.modeButtons, function(button) {
+        button.addEventListener('click', that);
+      });
+    },
+
+    handleEvent: function(evt) {
+      var target = evt.target;
+      if (evt.type === 'click' && target.classList.contains('mode-button')) {
+        var mode = target.id.replace('-button', '');
+        this.switchMode(mode);
+      }
+    },
+
+    switchMode: function(mode) {
+      document.body.dataset.mode = mode;
+      [].forEach.call(this.panelSections, function(section) {
+        if (section.id === mode) {
+          section.hidden = false;
+        } else {
+          section.hidden = true;
+        }
+      });
+      [].forEach.call(this.modeButtons, function(button) {
+        if (button.id === mode + '-button') {
+          button.disabled = 'disabled';
+        } else {
+          button.disabled = undefined;
+        }
+      });
+    }
+  };
+
   exports.app = new App();
+  exports.app.start();
 }(window));
